6ebed64cf64d02c234f19dbcaaced0f725c62421
Om Dandade <om.dandade@capgemini.com>
2024-06-17 11:04:13 +0530

    image loader resolved but same problem with css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
     mode: 'development',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/template.html'
-        })
+        }),
+        new CleanWebpackPlugin()
     ],
     module: {
         rules: [
             {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
+                ]
+            },
+            {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
-            }
+            },
+            // {
+            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            //     use: {
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: '[name].[hash].[ext]',
+            //             outputPath: 'images'
+            //         }
+            //     }
+            // }
         ]
     }
 }
